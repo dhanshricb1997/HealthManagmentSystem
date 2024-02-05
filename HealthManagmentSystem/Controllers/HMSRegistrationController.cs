f19464f24e8742146e75524e7e05348296184280
laxmikantbirajdar <laxmikantbirajdar@DELL-9MM3V93>
2024-02-05 16:53:37 +0530

    Add project files.

---
+++ HealthManagmentSystem/Controllers/HMSRegistrationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthManagmentSystem.Controllers
+{
+    public class HMSRegistrationController : Controller
+    {
+        // GET: HMSRegistration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult registartionForm()
+        {
+            return View();
+        }
+    }
+}
